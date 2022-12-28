bcc586641278ff909b24e6be66ae3793399344f8
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2022-12-27 22:07:48 -0800

    Generalized the DateWrapper

--- src/dateWrapper.ts
+++ src/dateWrapper.ts
@@ -7,35 +7,71 @@ export class DateWrapper {
 
     static dayToMs = DateWrapper.hourToMs * 24;
 
-    static roundQuarterHour(date: Date | number = new Date()) {
+    static flatUTCMinutes(
+        date: Date | number = new Date(),
+        flatFunc: (x: number) => number,
+        interval: number
+    ) {
         const returnDate = new Date(date);
-        const newMinutes = Math.round(returnDate.getUTCMinutes() / 15) * 15;
+        const newMinutes =
+            flatFunc(returnDate.getUTCMinutes() / interval) * interval;
         returnDate.setUTCMinutes(newMinutes, 0, 0);
         return returnDate;
     }
 
-    static floorUTCFiveMinutes(date: Date | number = new Date()) {
+    static flatUTCHours(
+        date: Date | number = new Date(),
+        flatFunc: (x: number) => number,
+        interval: number
+    ) {
         const returnDate = new Date(date);
-        const newMinutes = Math.floor(returnDate.getUTCMinutes() / 5) * 5;
-        returnDate.setUTCMinutes(newMinutes, 0, 0);
+        const newHours =
+            flatFunc(returnDate.getUTCHours() / interval) * interval;
+        returnDate.setUTCHours(newHours, 0, 0, 0);
+        return returnDate;
+    }
+
+    static flatUTCDays(
+        date: Date | number = new Date(),
+        flatFunc: (x: number) => number,
+        interval: number
+    ) {
+        const returnDate = new Date(date);
+        const newDays = flatFunc(returnDate.getUTCDate() / interval) * interval;
+        returnDate.setUTCDate(newDays);
+        returnDate.setUTCHours(0, 0, 0, 0);
         return returnDate;
     }
 
     static floorUTCMinutes(date: Date | number = new Date()) {
-        const returnDate = new Date(date);
-        returnDate.setUTCSeconds(0, 0);
-        return returnDate;
+        return DateWrapper.flatUTCMinutes(date, Math.floor, 1);
+    }
+
+    static roundUTCMinutes(date: Date | number = new Date()) {
+        return DateWrapper.flatUTCMinutes(date, Math.round, 1);
+    }
+
+    static floorUTCFiveMinutes(date: Date | number = new Date()) {
+        return DateWrapper.flatUTCMinutes(date, Math.floor, 5);
+    }
+
+    static roundUTCFiveMinutes(date: Date | number = new Date()) {
+        return DateWrapper.flatUTCMinutes(date, Math.round, 5);
     }
 
     static floorUTCHours(date: Date | number = new Date()) {
-        const returnDate = new Date(date);
-        returnDate.setUTCMinutes(0, 0, 0);
-        return returnDate;
+        return DateWrapper.flatUTCHours(date, Math.floor, 1);
+    }
+
+    static roundUTCHours(date: Date | number = new Date()) {
+        return DateWrapper.flatUTCHours(date, Math.round, 1);
     }
 
     static floorUTCDays(date: Date | number = new Date()) {
-        const returnDate = new Date(date);
-        returnDate.setUTCHours(0, 0, 0, 0);
-        return returnDate;
+        return DateWrapper.flatUTCDays(date, Math.floor, 1);
+    }
+
+    static roundUTCDays(date: Date | number = new Date()) {
+        return DateWrapper.flatUTCDays(date, Math.round, 1);
     }
 }
